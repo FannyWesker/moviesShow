943e4f0a64f7e9feb59148ac16450fab0a62ade9
Fanny Wesker <Fanny.Wesker@gmail.com>
2017-01-06 11:29:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# moviesShow
+  This is a practice project on film presentation and search. Do not good enough, please exhibitions.
---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+  init();
+});
+
+function init(){
+  $('.dropdown-toggle').dropdown();
+  //发请求获取json，再把这个json写入默认参数中。
+}
+
+
+// function Sortshow(){
+  // this.settings = {
+    // //默认参数
+  // };
+// }
+
+// Sortshow.prototype.frist = function(opt){
+  // extend(this.settings,opt);
+// };
+
+// function extend(obj1,obj2){
+  // for(var attr in obj2){
+    // obj1[attr] = obj2[attr];
+  // }
+// }
